e42859141a03f657c4f7360f09b0d527f1a920e0
Chris Vanndy <2736@holbertonschool.com>
2021-10-12 13:37:45 -0500

    recursively print tree left to right

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - prints binary tree in logical order.
+ * @tree: pointer to root node of tree to print
+ * @func: function to determin order
+ * Return: 0 (Success)
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	
+	/* print the root nodes ->n value */
+	(func)(tree->n);
+	/* recursively print left / right node->n value */
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
