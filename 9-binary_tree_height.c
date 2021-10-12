2e6a97ce4ba804cb60807601af85c1940e1b5de7
Chris Vanndy <2736@holbertonschool.com>
2021-10-12 15:28:14 -0500

    return height of tree from node

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - prints binary tree height.
+ * @tree: pointer to root node of tree to print
+ * Return: 0 (Success)
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree || (tree->right == NULL && tree->left == NULL))
+		return (0);
+	
+	size_t rHeight = binary_tree_height(tree->right);
+	size_t lHeight = binary_tree_height(tree->left);
+
+	if (rHeight > lHeight)
+		return (rHeight + 1);
+	else
+		return (lHeight + 1);
+
+}
--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,7 @@ void print_num(int n);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif
 
