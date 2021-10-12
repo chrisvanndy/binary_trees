c0a99f7e5b3aac8f312416be05dd51c0da66591c
Chris Vanndy <2736@holbertonschool.com>
2021-10-12 15:30:43 -0500

    pushing again

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - prints binary tree height.
+ * binary_tree_height - prints binary tree height from node.
  * @tree: pointer to root node of tree to print
  * Return: 0 (Success)
  */
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree || (tree->right == NULL && tree->left == NULL))
 		return (0);
-	
+
 	size_t rHeight = binary_tree_height(tree->right);
 	size_t lHeight = binary_tree_height(tree->left);
 
