722c28a475b4cbf476af5281492d347c2756eb9b
Chris Vanndy <2736@holbertonschool.com>
2021-10-13 14:43:08 -0500

    pushing 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - returns the value of sibling node
+ * @node: pointer to root node of tree to print
+ * Return: value at sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->right == NULL || node->parent->left == NULL)
+		return (NULL);
+
+	if (node->parent->left->n == node->n)
+	{
+		return (node->parent->right);
+	}
+
+	return (node->parent->left);
+
+}
