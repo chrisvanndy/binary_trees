2bc3b9eb90fe638505d6e7dcb755ee9abb4ca715
Chris Vanndy <2736@holbertonschool.com>
2021-10-13 18:35:58 -0500

    reverting changes to 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -73,11 +73,9 @@ int check_perf(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-		
+	
 	if (binary_tree_balance(tree) == 0 && check_perf(tree))
 		return (1);
-
-	return (0);
+	else
+		return (0);
 }
